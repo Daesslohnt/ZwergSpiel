664dd46e48bf2f3775a7a3c578daab3a0fed58ed
Anager <fridrich1900@gmail.com>
2022-01-18 18:15:22 +0100

    added launcher

---
+++ launcher.py
@@ -0,0 +1,13 @@
+from utils.json_parser import JsonParser
+from game_board.interface_v2 import Interface
+
+config = JsonParser.parse()
+
+
+if __name__ == '__main__':
+    interface = Interface()
+    interface.set_up_screen(config["screen_size"], config["screen_size"])
+    interface.set_board()
+    interface.create_all_elements(config["count_of_gold"], config["count_of_kobolds"])
+    interface.set_game_logic()
+    interface.game_action()
--- utils/json_parser.py
+++ utils/json_parser.py
@@ -5,7 +5,7 @@ class JsonParser():
 
     @staticmethod
     def parse():
-        with open("../sorce/configuration.json") as f:
+        with open("sorce/configuration.json") as f:
             data = f.read()
             data = json.loads(data)
             return data
